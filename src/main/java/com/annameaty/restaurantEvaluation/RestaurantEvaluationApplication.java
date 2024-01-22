ea089f04a84b4cb21509502f03c6d0133901afdc
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2024-01-22 11:23:14 -0500

    feature project init

---
+++ src/main/java/com/annameaty/restaurantEvaluation/RestaurantEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.annameaty.restaurantEvaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annameaty/restaurantEvaluation/RestaurantEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annameaty.restaurantEvaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
